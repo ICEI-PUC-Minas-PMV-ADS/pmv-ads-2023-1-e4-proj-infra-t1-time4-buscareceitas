c21ea8c7e801628ac5b91928de21487c2642f161
Renatadiniz <90404960+Renatadiniz@users.noreply.github.com>
2023-06-07 11:36:04 -0300

    Update MainNavigation.js

--- mobile/src/Routes/MainNavigation.js
+++ mobile/src/Routes/MainNavigation.js
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../Screens/Login/index";
 import Cadastro from "../Screens/Cadastro/index";
-import MeuPerfil from "../Screens/MeuPerfil/index":
+import MeuPerfil from "../Screens/MeuPerfil/index";
 import MinhasReceitas from "../Screens/MinhasReceitas/index";
 
 
